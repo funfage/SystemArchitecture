0c7b5060d76f56c11cd54b19e183fe271daf1e0b
yunfat_zhow <515207732@qq.com>
2020-06-25 21:55:55 +0800

    文件上传功能实现

--- 课程设计-个人博客系统/blog/src/main/java/com/zrf/blog/controller/UploadController.java
+++ 课程设计-个人博客系统/blog/src/main/java/com/zrf/blog/controller/UploadController.java
@@ -1,5 +1,6 @@
 package com.zrf.blog.controller;
 
+import com.zrf.blog.enums.ResultEnum;
 import com.zrf.blog.utils.Result;
 import com.zrf.blog.utils.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
- * @author: 张润发
+ * @author 张润发
+ * @date 2020/6/10
  */
 @RestController
 @RequestMapping("/upload")
@@ -19,12 +21,14 @@ public class UploadController {
 
     /**
      * 上传文件
-     *
      * @param file
      * @return
      */
     @RequestMapping("/uploadImage")
     public Result<String> uploadImage(MultipartFile file) {
+        if (file == null) {
+            return new Result<>(ResultEnum.ERROR.getCode(), "图片文件为空！");
+        }
         String url = uploadService.uploadImage(file);
         return new Result<>("上传成功！", url);
     }

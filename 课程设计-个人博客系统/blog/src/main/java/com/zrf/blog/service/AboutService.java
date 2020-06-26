4570ee2cfc71d0fe7282b9969d23673952785450
yunfat_zhow <515207732@qq.com>
2020-06-26 10:05:41 +0800

    完成关于我的阅读查询和删除

--- 课程设计-个人博客系统/blog/src/main/java/com/zrf/blog/service/AboutService.java
+++ 课程设计-个人博客系统/blog/src/main/java/com/zrf/blog/service/AboutService.java
@@ -1,6 +1,7 @@
 package com.zrf.blog.service;
 
 import com.zrf.blog.pojo.About;
+import com.zrf.blog.utils.Page;
 
 /**
  * @author 张润发
@@ -19,4 +20,30 @@ public interface AboutService {
      * @param about
      */
     void update(About about);
+
+    /**
+     * 根据id获取
+     * @param id
+     * @return
+     */
+    About getById(Integer id);
+
+    /**
+     * 阅读
+     * @return
+     */
+    About read();
+
+    /**
+     * 根据id删除
+     * @param id
+     */
+    void deleteById(Integer id);
+
+    /**
+     * 分页查询
+     * @param page
+     * @return
+     */
+    Page<About> getByPage(Page<About> page);
 }
--- 课程设计-个人博客系统/blog/src/main/java/com/zrf/blog/service/impl/AboutServiceImpl.java
+++ 课程设计-个人博客系统/blog/src/main/java/com/zrf/blog/service/impl/AboutServiceImpl.java
@@ -5,8 +5,11 @@ import com.zrf.blog.exception.BlogException;
 import com.zrf.blog.mapper.AboutMapper;
 import com.zrf.blog.pojo.About;
 import com.zrf.blog.service.AboutService;
+import com.zrf.blog.utils.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author 张润发
@@ -29,6 +32,39 @@ public class AboutServiceImpl implements AboutService {
     @Override
     public void update(About about) {
         aboutMapper.updateById(about);
+    }
+
+    @Override
+    public About getById(Integer id) {
+        return aboutMapper.getById(id);
+    }
+
+    @Override
+    public About read() {
+        About about = aboutMapper.getAbout();
+        if (about == null) {
+            return null;
+        }
+        // 更新阅读数
+        about.setAboutRead(about.getAboutRead() + 1);
+        aboutMapper.updateById(about);
+        return about;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        aboutMapper.deleteById(id);
+    }
+
+    @Override
+    public Page<About> getByPage(Page<About> page) {
+        // 查询数据
+        List<About> aboutList = aboutMapper.getByPage(page);
+        page.setList(aboutList);
+        // 查询总数
+        int totalCount = aboutMapper.getCountByPage(page);
+        page.setTotalCount(totalCount);
+        return page;
     }
 
 }

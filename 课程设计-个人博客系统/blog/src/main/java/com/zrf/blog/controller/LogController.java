372b2f6c940fd690b139b9eb166332a483265c0d
yunfat_zhow <515207732@qq.com>
2020-06-27 10:12:12 +0800

    完成日志的导出和批量删除

--- 课程设计-个人博客系统/blog/src/main/java/com/zrf/blog/controller/LogController.java
+++ 课程设计-个人博客系统/blog/src/main/java/com/zrf/blog/controller/LogController.java
@@ -6,9 +6,11 @@ import com.zrf.blog.service.LogService;
 import com.zrf.blog.utils.Page;
 import com.zrf.blog.utils.Result;
 import com.zrf.blog.utils.StringUtils;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
 
@@ -54,6 +56,35 @@ public class LogController {
     public Result<Object> delete(@PathVariable Integer id) {
         logService.deleteById(id);
         return new Result<>("删除成功！");
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param ids
+     * @return
+     */
+    @DeleteMapping(value = "/deleteByIds")
+    public Result<Object> deleteByIds(@RequestBody List<Integer> ids) {
+        if (ids.size() == 0) {
+            return new Result<>(ResultEnum.ERROR.getCode(), "请选择需要删除的日志！");
+        }
+        logService.deleteByIds(ids);
+        return new Result<>("删除成功！");
+    }
+
+    /**
+     * 全部导出
+     *
+     * @throws Exception
+     */
+    @GetMapping(value = "/export")
+    public void export(HttpServletResponse response) throws Exception {
+        Workbook workbook = logService.export();
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + "日志");
+        response.flushBuffer();
+        workbook.write(response.getOutputStream());
     }
 
 
